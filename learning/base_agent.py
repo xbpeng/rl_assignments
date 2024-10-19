a7a90357bb67c561354bf1128c500eee9823c7e2
xbpeng <JasonPeng142@hotmail.com>
2024-10-18 20:09:08 -0700

    Update base_agent.py

--- learning/base_agent.py
+++ learning/base_agent.py
@@ -59,11 +59,12 @@ class BaseAgent(torch.nn.Module):
         while self._sample_count < max_samples:
             train_info = self._train_iter()
             
-            output_iter = (self._iter % self._iters_per_output == 0)
+            self._sample_count = self._update_sample_count()
+            output_iter = (self._iter % self._iters_per_output == 0) or (self._sample_count >= max_samples)
+
             if (output_iter):
                 test_info = self.test_model(self._test_episodes)
             
-            self._sample_count = self._update_sample_count()
             self._log_train_info(train_info, test_info, start_time) 
             self._logger.print_log()
 
